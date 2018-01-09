7c8c5fc92c1c52892e643db004d2e1fe25353951
Marx JMoura <marx.jmoura@gmail.com>
2018-01-09 19:49:58 -0300

    Sidebar documentation and small code changes

--- preview/assets/js/admin4bs.preview.js
+++ preview/assets/js/admin4bs.preview.js
@@ -1,7 +1,7 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('pre').each(function(i, block) {
+    $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
     });
 });
