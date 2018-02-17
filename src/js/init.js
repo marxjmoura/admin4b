5e660769500d3b401c3aa737f9fd2d061f2cb7d4
Marx JMoura <marx.jmoura@gmail.com>
2018-02-17 11:51:18 -0300

    .input-group treated as input

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,6 @@
 $(function () {
   $('input').typing();
+  $('.as-input').asInput();
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="suggestion"]').suggestion();
