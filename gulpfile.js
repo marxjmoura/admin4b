9a4bef0f4b940440dc5cc0b72cf9abd221ef2098
Marx JMoura <marx.jmoura@gmail.com>
2018-04-22 20:21:57 -0300

    Minify HTML compiled to docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const fileinclude = require('gulp-file-include');
 const htmlextend = require('gulp-html-extend')
+const htmlmin = require('gulp-htmlmin');
 const rename = require("gulp-rename");
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
@@ -32,6 +33,7 @@ gulp.task('build-html', () => {
             prefix: '@@',
             basepath: './src/html/'
         }).on('error', (e) => console.log(e)))
+        .pipe(htmlmin({ collapseWhitespace: true }).on('error', (e) => console.log(e)))
         .pipe(gulp.dest('docs/'));
 });
 
