db0199e1bfc0f2484094d7854f9918b725f84f63
Marx JMoura <marx.jmoura@gmail.com>
2018-04-24 22:48:01 -0300

    Small improvement in documentation for input date

--- src/html/includes/code-examples/input-controls/input-date/input-date.js
+++ src/html/includes/code-examples/input-controls/input-date/input-date.js
@@ -1,17 +1,17 @@
 var $calendar = $('#calendar');
-var $btnApplyDate = $('#btnApplyDate')
+var $btnApply = $('#btnApply')
 
 $calendar.on('show.bs.modal', function (e) {
   var $formControl = $(e.relatedTarget)
     .closest('.form-group')
     .find('.form-control');
 
-  $btnApplyDate.prop('target', $formControl);
+  $btnApply.prop('target', $formControl);
   $calendar.calendar('date', $formControl.prop('date') || new Date());
 });
 
-$btnApplyDate.on('click', function () {
-  var $target = $btnApplyDate.prop('target');
+$btnApply.on('click', function () {
+  var $target = $btnApply.prop('target');
   var date = $calendar.calendar('date');
   var formattedDate = moment(date).format('dddd, MMMM D, YYYY');
 
