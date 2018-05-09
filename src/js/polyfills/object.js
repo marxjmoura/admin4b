0388c8c30d369bb5188258c83ed7ae0edd31573b
Marx JMoura <marx.jmoura@gmail.com>
2018-05-09 06:07:39 -0300

    Fix object polyfill

--- src/js/polyfills/object.js
+++ src/js/polyfills/object.js
@@ -1,12 +1,16 @@
 (function () {
 
   var userAgent = window.navigator.userAgent;
-  var ie10 = userAgent.indexOf("MSIE ") > 0;
-  var ie11 = userAgent.indexOf('Trident/');
+  var isIE10 = userAgent.indexOf("MSIE ") > 0;
+  var isIE11 = userAgent.indexOf('Trident/') > 0;
 
-  if (ie10 || ie11) {
+  if (isIE10 || isIE11) {
     Object.keys = function (obj) {
       var keys = [];
+
+      if (!obj) {
+        return keys;
+      }
 
       for (var p in obj) {
         if (obj.hasOwnProperty(p)) {
