d0bd8b579436fe6f1faa8b62c61f9ace642d9f1f
Marx JMoura <marx.jmoura@gmail.com>
2018-05-18 07:15:14 -0300

    Add js methods for sidebar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,18 +1,23 @@
 (function ($) {
 
-  $.fn.sidebar = function () {
+  $.fn.sidebar = function (event) {
     return this.each(function () {
       var $sidebar = $(this);
+
+      switch (event) {
+        case 'show': $sidebar.addClass('show'); return;
+        case 'hide': $sidebar.removeClass('show'); return;
+      }
 
       // Prevent duplicate events if called more than once
       if ($sidebar.prop('initialized')) return;
 
       $('[data-toggle="sidebar"]').on('click', function () {
-        $sidebar.addClass('sidebar-open');
+        $sidebar.sidebar('show');
       });
 
       $('[data-dismiss="sidebar"]').on('click', function () {
-        $sidebar.removeClass('sidebar-open');
+        $sidebar.sidebar('hide');
       });
 
       $sidebar.prop('initialized', true);
