fa18a58385b6d62897c4d2c5fc9f61cd7afc179f
Marx JMoura <marx.jmoura@gmail.com>
2019-01-14 11:12:39 -0300

    Use InOut.js to validate file size

--- src/js/file-manager/file-manager-maxsize.js
+++ src/js/file-manager/file-manager-maxsize.js
@@ -1,5 +1,6 @@
+import io from 'inoutjs'
+
 const sizeRegex = /^(\d*\.?\d+)(B|KB|MB|GB)?$/gi
-const exponentOf = { B: 0, KB: 1, MB: 2, GB: 3 }
 
 class MaxsizeValidation {
   constructor(file) {
@@ -14,11 +15,7 @@ class MaxsizeValidation {
     const size = matches[1]
     const unit = matches[2]
 
-    return this._file.size <= this._toBytes(size, unit)
-  }
-
-  _toBytes(size, unit) {
-    return Number(size) * Math.pow(1024, exponentOf[unit])
+    return io(this._file).lowerOrEqual(size, unit)
   }
 }
 
